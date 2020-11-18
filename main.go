d6fd1cf541e3ad96378c57b0e43b034c7a49a1e9
lwaly <279527404@qq.com>
2020-11-18 17:06:09 +0800

    增加http测试客户端

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net"
+	"net/http"
 	"strings"
 	"sync"
 	"time"
@@ -23,8 +27,15 @@ var wg sync.WaitGroup
 func main() {
 	// testEncrypt()
 	// Token_auth("token")
-	strList := common.Conf.GetKeyList("user")
+
 	mapUser = make(map[string]*User)
+	// go tcpTest()
+	httpTest()
+	wg.Wait()
+}
+
+func tcpTest() {
+	strList := common.Conf.GetKeyList("user")
 	//读取登录账号
 	for _, value := range strList {
 		if str, err := common.Conf.GetValue("user", value); nil == err {
@@ -39,20 +50,52 @@ func main() {
 		go func(user *User) {
 			defer wg.Done()
 			//连接网关
-			conn, err := net.Dial("tcp", "192.168.0.20:10008")
+			conn, err := net.Dial("tcp", "127.0.0.1:8480")
 			if nil != err {
 				fmt.Print("fail to connect", err)
 				return
 			}
 			//登录
 			user.privatekey, user.uid = UserLogin(conn, 1, user.phone, user.passwd)
-			for i := 0; i < 10000; i++ {
+			for i := 0; i < 10; i++ {
 				//循环获取手机区号
 				ZoneGet(conn, user.phone)
 				time.Sleep(time.Duration(3) * time.Second)
 			}
 		}(user)
 	}
-	wg.Wait()
 	fmt.Print(time.Now().Unix() - begin)
+}
+
+func httpTest() {
+	data := map[string]interface{}{
+		"page_size":  3,
+		"page_index": 1,
+		"id":         1,
+	}
+	b, _ := json.Marshal(data)
+
+	client := &http.Client{}
+
+	req, err := http.NewRequest("POST", "http://127.0.0.1:18480/a/b/c", bytes.NewReader(b))
+	if err != nil {
+		fmt.Print(err)
+		return
+	}
+
+	req.Header.Set("Token", Create_token(1, "ly"))
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Print(err)
+		return
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		fmt.Print(err)
+		return
+	}
+
+	fmt.Println(string(body))
 }
